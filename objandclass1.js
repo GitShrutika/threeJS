060f2d0f35eac89d3c7bb94eeaf7f35f59349a9a
Shrutika <shrutika.gitdesk@gmail.com>
2026-01-29 22:59:14 +0530

    Create objandclass1.js

---
+++ objandclass1.js
@@ -0,0 +1,19 @@
+const student={
+    fullName:"shrutika",
+    marks:84,
+    printMarks:function(){
+        console.log("marks = ",this.marks);
+    },
+};
+const employee={
+    calcTax1(){
+        console.log("tax rate is 10%");
+    },
+    calcTax2 : function () { //another method to write
+    console.log("tax rate is 10%")
+}
+};
+
+const karanArjun={
+    salary
+}
